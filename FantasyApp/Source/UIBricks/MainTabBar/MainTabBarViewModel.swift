2807450fa0036972210ee6c63ce4c45e80a6d3a7
Boris Vinogradov <borys.vynohradov@avast.com>
2019-08-14 14:07:54 +0300

    code review

--- FantasyApp/Source/UIBricks/MainTabBar/MainTabBarViewModel.swift
+++ FantasyApp/Source/UIBricks/MainTabBar/MainTabBarViewModel.swift
@@ -31,7 +31,7 @@ struct MainTabBarViewModel : MVVM_ViewModel {
      fileprivate let privateTextVar = BehaviourRelay<String?>(nil)
      
      */
-    private let locationActor: LocationActor = LocationActor()
+    private let locationActor = LocationActor()
     
     init(router: MainTabBarRouter) {
         self.router = router
