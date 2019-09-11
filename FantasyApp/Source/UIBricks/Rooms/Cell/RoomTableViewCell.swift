6696cf1c5f49ea8b83ba1eb43d5edb578b908ed4
Boris Vinogradov <borys.vynohradov@avast.com>
2019-09-11 09:57:07 +0300

    WIP

---
+++ FantasyApp/Source/UIBricks/Rooms/Cell/RoomTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  RoomTableViewCell.swift
+//  FantasyApp
+//
+//  Created by Admin on 10.09.2019.
+//  Copyright © 2019 Fantasy App. All rights reserved.
+//
+
+import UIKit
+
+class RoomTableViewCell: UITableViewCell {
+    @IBOutlet private (set) var nameLabel: UILabel!
+    @IBOutlet private (set) var timeLabel: UILabel!
+    @IBOutlet private (set) var lastMessageLabel: UILabel!
+}
