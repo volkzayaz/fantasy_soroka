77def150b528cda65398ff88d5766149ff3de4a7
Vlad Soroka <volkzayaz@gmail.com>
2019-11-26 13:34:01 +0200

    Fixed bug on not opening socket after relogin

--- FantasyApp/Source/WebSocketService.swift
+++ FantasyApp/Source/WebSocketService.swift
@@ -19,18 +19,15 @@ class WebSocketService {
     /////----------
     
     var didReceiveMessage: Observable<Room.MessageInRoom> {
-        return Observable.merge(messageProxy.notNil(), subscribe(onEvent: "message"))
+        return Observable.merge(messageProxy.skip(1).notNil(),
+                                manager.defaultSocket.rx.subscribe(onEvent: "message"))
     }
     
     func didReceiveMessage(in room: RoomIdentifier) -> Observable<Room.MessageInRoom> {
         return didReceiveMessage
             .filter { $0.roomId == room.id }
     }
-    
-    ///rule of sending own message to self via socket is in this proxy
-    ///in reality you should just use socket as transport
-    ///shame on you lazy ass =)
-    private let messageProxy = BehaviorSubject<Room.MessageInRoom?>(value: nil)
+
     func send(message: Room.MessageInRoom) -> Single<Room.MessageInRoom> {
         
         struct Confirmation: Codable {
@@ -38,7 +35,7 @@ class WebSocketService {
             let messageId: String?
         }
         
-        return send(event: "message", with: message)
+        return manager.defaultSocket.rx.send(event: "message", with: message)
             .map { (x: Confirmation) -> Room.MessageInRoom in
                 
                 guard let id = x.messageId else {
@@ -54,12 +51,16 @@ class WebSocketService {
             })
             
     }
+    ///rule of sending own message to self via socket is in this proxy
+    ///in reality you should just use socket as transport
+    ///shame on you lazy ass =)
+    private let messageProxy = BehaviorSubject<Room.MessageInRoom?>(value: nil)
     
     /////----------
     /////Implementation
     /////----------
     
-    private var socketManager: SocketManager!
+    private var manager: SocketManager!
     
     init() {
         
@@ -67,27 +68,98 @@ class WebSocketService {
         appState.map { $0.currentUser != nil }
             .distinctUntilChanged()
             .drive(onNext: { (userExist) in
-        
+
                 guard let t = PFUser.current()?.sessionToken else {
-                    self.socketManager = nil
-                    
+                    self.manager = nil
+
                     return
                 }
-                
-                self.socketManager = SocketManager(socketURL: URL(string: ServerURL.socket)!,
+
+                self.manager = SocketManager(socketURL: URL(string: ServerURL.socket)!,
                                                    config: [ .log(true), .forceNew(true), .connectParams(["token": t]) ])
-                
-                self.socketManager.connect()
-                
+                ///bug in the library
+                ///need to call it to lazily create default socketIOClient
+                _ = self.manager.defaultSocket
+                self.manager.connect()
+
             })
                 
     }
+//
+//    lazy var didConnect: Observable<Void> = {
+//
+//        return Observable.create { [unowned s = self.manager.defaultSocket] (subscriber) -> Disposable in
+//
+//            s.on(clientEvent: .connect, callback: { (data, ack: SocketAckEmitter) in
+//                subscriber.onNext( () )
+//            })
+//
+//            return Disposables.create {
+//                ///
+//            }
+//        }
+//        .share()
+//
+//    }()
+//
+//    lazy var didDisconnect: Observable<Void> = {
+//
+//        return Observable.create { [unowned s = self.manager.defaultSocket] (subscriber) -> Disposable in
+//
+//            s.on(clientEvent: .disconnect, callback: { (data, ack: SocketAckEmitter) in
+//                subscriber.onNext( () )
+//            })
+//
+//            return Disposables.create {
+//                ///
+//            }
+//        }
+//        .share()
+//
+//    }()
+    
+}
 
-    private func send<T: SocketData, U: Codable>(event: String, with data: T) -> Single<U> {
+extension SocketIOClient {
+    var rx: Reactive<SocketIOClient> {
+        return Reactive(self)
+    }
+}
+
+extension Reactive where Base == SocketIOClient {
+
+    fileprivate func subscribe<T: Codable>(onEvent name: String) -> Observable<T> {
+
+        return Observable.create { (subscriber) -> Disposable in
+
+            let uid = self.base.on(name) { (data: [Any]?, emt: SocketAckEmitter) in
+                
+                guard let dic = data?.first as? [String: Any],
+                    let bytes = try? JSONSerialization.data(withJSONObject: dic, options: []),
+                    let model = try? fantasyDecoder.decode(T.self, from: bytes) else {
+                        
+                    subscriber.onError( FantasyError.generic(description: "Can't parse response for socket event \(name)") )
+                    return
+                        
+                }
+                
+                subscriber.onNext(model)
+                
+            }
+            
+            
+            return Disposables.create {
+                self.base.off(id: uid)
+            }
+        }
+        
+    }
+    
+    fileprivate func send<T: SocketData, U: Codable>(event: String, with data: T) -> Single<U> {
         
         return Single.create { (subscriber) -> Disposable in
             
-            self.socketManager.defaultSocket.emitWithAck(event, data).timingOut(after: 1) { (data: [Any]) in
+            self.base.emitWithAck(event, data).timingOut(after: 1) { (data: [Any]) in
                 
                 guard let dic = data.first as? [String: Any],
                     let bytes = try? JSONSerialization.data(withJSONObject: dic, options: []),
@@ -106,64 +178,5 @@ class WebSocketService {
         
         
     }
-    
-    private func subscribe<T: Codable>(onEvent name: String) -> Observable<T> {
-
-        return Observable.create { [unowned s = self.socketManager.defaultSocket] (subscriber) -> Disposable in
-
-            let uid = s.on(name) { (data: [Any]?, emt: SocketAckEmitter) in
-                
-                guard let dic = data?.first as? [String: Any],
-                    let bytes = try? JSONSerialization.data(withJSONObject: dic, options: []),
-                    let model = try? fantasyDecoder.decode(T.self, from: bytes) else {
-                        
-                    subscriber.onError( FantasyError.generic(description: "Can't parse response for socket event \(name)") )
-                    return
-                        
-                }
-                
-                subscriber.onNext(model)
-                
-            }
-            
-            
-            return Disposables.create {
-                s.off(id: uid)
-            }
-        }
-        
-    }
-    
-    lazy var didConnect: Observable<Void> = {
-        
-        return Observable.create { [unowned s = self.socketManager.defaultSocket] (subscriber) -> Disposable in
-            
-            s.on(clientEvent: .connect, callback: { (data, ack: SocketAckEmitter) in
-                subscriber.onNext( () )
-            })
-            
-            return Disposables.create {
-                ///
-            }
-        }
-        .share()
-        
-    }()
-    
-    lazy var didDisconnect: Observable<Void> = {
-        
-        return Observable.create { [unowned s = self.socketManager.defaultSocket] (subscriber) -> Disposable in
-            
-            s.on(clientEvent: .disconnect, callback: { (data, ack: SocketAckEmitter) in
-                subscriber.onNext( () )
-            })
-            
-            return Disposables.create {
-                ///
-            }
-        }
-        .share()
-        
-    }()
     
 }
