e191b369d9a033569fbcfa25387d9d420ed56a5d
Anatolii Afanasiev <anatolii.afanasiev@globallogic.com>
2019-12-09 23:33:31 +0200

    changed photo picker library.

--- FantasyApp/Source/Tools/UI/ImagePicker.swift
+++ FantasyApp/Source/Tools/UI/ImagePicker.swift
@@ -7,69 +7,54 @@
 //
 
 import Foundation
-import FMPhotoPicker
+import YPImagePicker
 
 protocol ImagePicker {
     static func present(on viewController: UIViewController, completion: @escaping (UIImage) -> Void)
 }
 
-class FMPhotoImagePicker: ImagePicker, FMPhotoPickerViewControllerDelegate {
-    
-    static let delegate = FMPhotoImagePicker()
-    
+class FMPhotoImagePicker: ImagePicker {
+
     private var completion: ((UIImage) -> Void)? = nil
     
     static func present(on viewController: UIViewController, completion: @escaping (UIImage) -> Void) {
-        
-        FMPhotoImagePicker.delegate.completion = completion
-        
-        var config = FMPhotoPickerConfig()
-        config.selectMode = .single
-        config.availableCrops = [FMCrop.ratioCustom]
-        config.availableFilters = [FMFilter.None]
 
-        config.strings = [
-            "picker_warning_over_image_select_format": "\(R.string.localizable.fantasyImagePickerLibraryPicker_warning_over_image_select_formatPart1()) %d \(R.string.localizable.generalImages())" ,
-            "picker_warning_over_video_select_format": "\(R.string.localizable.fantasyImagePickerLibraryPicker_warning_over_video_select_formatPart1()) %d \(R.string.localizable.generalVideos())" ,
+        UINavigationBar.appearance().tintColor = .fantasyPink
 
-            "picker_button_cancel": R.string.localizable.generalCancel(),
-            "picker_button_select_done": R.string.localizable.generalDone(),
+        var config = YPImagePickerConfiguration()
 
-            "present_button_back": R.string.localizable.generalBack(),
-            "present_button_edit_image": R.string.localizable.generalEdit(),
-            "editor_button_cancel": R.string.localizable.generalCancel(),
-            "editor_button_done": R.string.localizable.generalDone(),
-            "editor_menu_filter": R.string.localizable.fantasyImagePickerLibraryEditor_menu_filter(),
-            "editor_menu_crop": R.string.localizable.fantasyImagePickerLibraryEditor_menu_crop(),
-            "editor_menu_crop_button_reset": R.string.localizable.fantasyImagePickerLibraryEditor_menu_crop_button_reset(),
-            "editor_menu_crop_button_rotate": R.string.localizable.fantasyImagePickerLibraryEditor_menu_crop_button_rotate(),
-            "editor_crop_ratioCustom": R.string.localizable.fantasyImagePickerLibraryEditor_crop_ratioCustom(),
-            "editor_crop_ratioOrigin": R.string.localizable.fantasyImagePickerLibraryEditor_crop_ratioOrigin(),
-            "editor_crop_ratioSquare": R.string.localizable.fantasyImagePickerLibraryEditor_crop_ratioSquare(),
-            "permission_dialog_title": R.string.localizable.fantasyImagePickerLibraryPermission_dialog_title(),
-            "permission_dialog_message": R.string.localizable.fantasyImagePickerLibraryPermission_dialog_message(),
-            "permission_button_ok": R.string.localizable.generalOk(),
-            "permission_button_cancel": R.string.localizable.generalCancel(),
-            "editor_crop_ratio4x3": "4:3",
-            "editor_crop_ratio16x9": "16:9",
-            "editor_crop_ratio9x16": "9x16",
-            "present_title_photo_created_date_format":  "yyyy/M/d",
-        ]
+        var colors = config.colors
+        colors.navigationBarActivityIndicatorColor = .fantasyPink
+        colors.tintColor = .fantasyPink
 
-        let picker = FMPhotoPickerViewController(config: config)
-        picker.delegate = FMPhotoImagePicker.delegate
+        config.colors = colors
+        config.library.mediaType = .photo
+        config.showsPhotoFilters = true
+        config.shouldSaveNewPicturesToAlbum = false
+        config.startOnScreen = .library
+        config.screens = [.library, .photo]
+        config.library.maxNumberOfItems = 1
+        config.hidesStatusBar = true
+
+        let picker = YPImagePicker(configuration: config)
+        picker.didFinishPicking { [unowned picker] items, cancelled in
+
+            UINavigationBar.appearance().tintColor = .white
+
+            if cancelled {
+                print("Picker was canceled")
+                picker.dismiss(animated: true, completion: nil)
+                return
+            }
+
+            let originalImage = items.singlePhoto?.originalImage
+            let modifiedImage = items.singlePhoto?.modifiedImage
+
+            completion(modifiedImage ?? originalImage!)
+            picker.dismiss(animated: true, completion: nil)
+        }
+
         viewController.present(picker, animated: true)
     }
-    
-    func fmPhotoPickerController(_ picker: FMPhotoPickerViewController, didFinishPickingPhotoWith photos: [UIImage]) {
-        
-        completion?(photos.first!)
-        self.completion = nil
-        picker.dismiss(animated: true, completion: {
-            picker.dismiss(animated: true, completion: nil)
-        })
-        
-    }
-    
     
 }
